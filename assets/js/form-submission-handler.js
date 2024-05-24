c7beb2d9234c8e9e0015200148f1637cf36f7a0d
Arun Kumar <arun79098@gmail.com>
2024-05-24 20:55:44 +0530

    domain restriction

--- assets/js/form-submission-handler.js
+++ assets/js/form-submission-handler.js
@@ -1,34 +1,28 @@
-(function() {
-  // get all data in form and return object
+(function () {
   function getFormData(form) {
     var elements = form.elements;
     var honeypot;
 
-    var fields = Object.keys(elements).filter(function(k) {
+    var fields = Object.keys(elements).filter(function (k) {
       if (elements[k].name === "honeypot") {
         honeypot = elements[k].value;
         return false;
       }
       return true;
-    }).map(function(k) {
-      if(elements[k].name !== undefined) {
+    }).map(function (k) {
+      if (elements[k].name !== undefined) {
         return elements[k].name;
-      // special case for Edge's html collection
-      } else if(elements[k].length > 0){
+      } else if (elements[k].length > 0) {
         return elements[k].item(0).name;
       }
-    }).filter(function(item, pos, self) {
+    }).filter(function (item, pos, self) {
       return self.indexOf(item) == pos && item;
     });
 
     var formData = {};
-    fields.forEach(function(name){
+    fields.forEach(function (name) {
       var element = elements[name];
-      
-      // singular form elements just have one value
       formData[name] = element.value;
-
-      // when our element has multiple items, get their values
       if (element.length) {
         var data = [];
         for (var i = 0; i < element.length; i++) {
@@ -41,67 +35,75 @@
       }
     });
 
-    // add form-specific values into the data
     formData.formDataNameOrder = JSON.stringify(fields);
-    formData.formGoogleSheetName = form.dataset.sheet || "responses"; // default sheet name
-    formData.formGoogleSendEmail = form.dataset.email || ""; // no email by default
+    formData.formGoogleSheetName = form.dataset.sheet || "responses";
+    formData.formGoogleSendEmail = form.dataset.email || "";
 
-    return {data: formData, honeypot: honeypot};
+    return { data: formData, honeypot: honeypot };
   }
 
-  function handleFormSubmit(event) {  // handles form submit without any jquery
-    event.preventDefault();           // we are submitting via xhr below
+  function handleFormSubmit(event) {
+    event.preventDefault();
     var form = event.target;
     var formData = getFormData(form);
     var data = formData.data;
 
-    // If a honeypot field is filled, assume it was done so by a spam bot.
     if (formData.honeypot) {
       return false;
     }
 
     disableAllButtons(form);
-    //var url = form.action;
-    var url ="https://script.google.com/macros/s/AKfycbwyx515kPDRF_x3uK4DP0eprFgFBEKNRQRJqXmDgt8a9rZSeNod8GIShQl45vH14LPu/exec";
 
+    fetch('https://api.ipify.org?format=json')
+      .then(response => response.json())
+      .then(ipData => {
+        data.ipAddress = ipData.ip;
+        sendFormData(form, data);
+      })
+      .catch(error => {
+        console.error('Error fetching IP address:', error);
+        sendFormData(form, data);
+      });
+  }
+
+  function sendFormData(form, data) {
+    var url = "https://script.google.com/macros/s/AKfycbwyx515kPDRF_x3uK4DP0eprFgFBEKNRQRJqXmDgt8a9rZSeNod8GIShQl45vH14LPu/exec";
     var xhr = new XMLHttpRequest();
     xhr.open('POST', url);
-    // xhr.withCredentials = true;
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.onreadystatechange = function() {
-      ;
-        if (xhr.readyState === 4 && xhr.status === 200) {
-          form.reset();
-          document.getElementById("contact_form").classList.remove("opacity");
-          document.getElementById("contact_overlay").classList.add("display-none");          // Animation logic here
-          document.getElementById("form_title").classList.add("display-none");
-          document.getElementById("form").classList.add("display-none");
-          document.getElementById("thanks_message").classList.remove("display-none");
 
-          setTimeout(() => {
-            document.getElementById("form_title").classList.remove("display-none");
-            document.getElementById("form").classList.remove("display-none");
-            document.getElementById("thanks_message").classList.add("display-none");
-          }, 3000);
-        }
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        form.reset();
+        document.getElementById("contact_form").classList.remove("opacity");
+        document.getElementById("contact_overlay").classList.add("display-none");
+        document.getElementById("form_title").classList.add("display-none");
+        document.getElementById("form").classList.add("display-none");
+        document.getElementById("thanks_message").classList.remove("display-none");
+
+        setTimeout(() => {
+          document.getElementById("form_title").classList.remove("display-none");
+          document.getElementById("form").classList.remove("display-none");
+          document.getElementById("thanks_message").classList.add("display-none");
+        }, 3000);
+      }
     };
 
     document.getElementById("contact_form").classList.add("opacity");
-document.getElementById("contact_overlay").classList.remove("display-none");
-    // url encode form data for sending as post data
-    var encoded = Object.keys(data).map(function(k) {
-        return encodeURIComponent(k) + "=" + encodeURIComponent(data[k]);
+    document.getElementById("contact_overlay").classList.remove("display-none");
+
+    var encoded = Object.keys(data).map(function (k) {
+      return encodeURIComponent(k) + "=" + encodeURIComponent(data[k]);
     }).join('&');
     xhr.send(encoded);
   }
-  
+
   function loaded() {
-    // bind to the submit event of our form
     var forms = document.querySelectorAll("form.gform");
     for (var i = 0; i < forms.length; i++) {
       forms[i].addEventListener("submit", handleFormSubmit, false);
     }
-  };
+  }
   document.addEventListener("DOMContentLoaded", loaded, false);
 
   function disableAllButtons(form) {
