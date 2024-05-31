3c00ff3679938de0daa61bf82ff3bd9ce024818e
Arun Kumar <arun79098@gmail.com>
2024-05-31 16:08:53 +0530

    URL change

--- assets/js/form-submission-handler.js
+++ assets/js/form-submission-handler.js
@@ -1,107 +1,107 @@
 (function () {
   function getFormData(form) {
-    var elements = form.elements;
-    var honeypot;
+      var elements = form.elements;
+      var honeypot;
 
-    var fields = Object.keys(elements).filter(function (k) {
-      if (elements[k].name === "honeypot") {
-        honeypot = elements[k].value;
-        return false;
-      }
-      return true;
-    }).map(function (k) {
-      if (elements[k].name !== undefined) {
-        return elements[k].name;
-      } else if (elements[k].length > 0) {
-        return elements[k].item(0).name;
-      }
-    }).filter(function (item, pos, self) {
-      return self.indexOf(item) == pos && item;
-    });
-
-    var formData = {};
-    fields.forEach(function (name) {
-      var element = elements[name];
-      formData[name] = element.value;
-      if (element.length) {
-        var data = [];
-        for (var i = 0; i < element.length; i++) {
-          var item = element.item(i);
-          if (item.checked || item.selected) {
-            data.push(item.value);
+      var fields = Object.keys(elements).filter(function (k) {
+          if (elements[k].name === "honeypot") {
+              honeypot = elements[k].value;
+              return false;
           }
-        }
-        formData[name] = data.join(', ');
-      }
-    });
+          return true;
+      }).map(function (k) {
+          if (elements[k].name !== undefined) {
+              return elements[k].name;
+          } else if (elements[k].length > 0) {
+              return elements[k].item(0).name;
+          }
+      }).filter(function (item, pos, self) {
+          return self.indexOf(item) == pos && item;
+      });
 
-    formData.formDataNameOrder = JSON.stringify(fields);
-    formData.formGoogleSendEmail = form.dataset.email || "";
+      var formData = {};
+      fields.forEach(function (name) {
+          var element = elements[name];
+          formData[name] = element.value;
+          if (element.length) {
+              var data = [];
+              for (var i = 0; i < element.length; i++) {
+                  var item = element.item(i);
+                  if (item.checked || item.selected) {
+                      data.push(item.value);
+                  }
+              }
+              formData[name] = data.join(', ');
+          }
+      });
 
-    console.log('Form data:', formData);
-    return { data: formData, honeypot: honeypot };
+      formData.formDataNameOrder = JSON.stringify(fields);
+      formData.formGoogleSendEmail = form.dataset.email || "";
+
+      console.log('Form data:', formData);
+      return { data: formData, honeypot: honeypot };
   }
 
   function handleFormSubmit(event) {
-    event.preventDefault();
-    var form = event.target;
-    var formData = getFormData(form);
-    var data = formData.data;
+      event.preventDefault();
+      var form = event.target;
+      var formData = getFormData(form);
+      var data = formData.data;
 
-    if (formData.honeypot) {
-      return false;
-    }
-
-    disableAllButtons(form);
-
-    var url = "https://script.google.com/macros/s/AKfycbyVrobcCBRdjjLfwhgRsGI9uR3vEwg4rCL8DWwsge3L6Jh1IWyyYnCGKgNFhsFjRsCp/exec";
-    var sheetName = form.dataset.sheet || "responses";
-
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', url);
-
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState === 4 && xhr.status === 200) {
-        console.log('Form submitted successfully:', xhr.responseText);
-        form.reset();
-        document.getElementById("contact_form").classList.remove("opacity");
-        document.getElementById("contact_overlay").classList.add("display-none");
-        document.getElementById("form_title").classList.add("display-none");
-        document.getElementById("form").classList.add("display-none");
-        document.getElementById("thanks_message").classList.remove("display-none");
-
-        setTimeout(() => {
-          document.getElementById("form_title").classList.remove("display-none");
-          document.getElementById("form").classList.remove("display-none");
-          document.getElementById("thanks_message").classList.add("display-none");
-        }, 3000);
-      } else if (xhr.readyState === 4) {
-        console.error('Error submitting form:', xhr.status, xhr.statusText);
+      if (formData.honeypot) {
+          return false;
       }
-    };
 
-    document.getElementById("contact_form").classList.add("opacity");
-    document.getElementById("contact_overlay").classList.remove("display-none");
+      disableAllButtons(form);
 
-    var encoded = Object.keys(data).map(function (k) {
-      return encodeURIComponent(k) + "=" + encodeURIComponent(data[k]);
-    }).join('&');
-    console.log('Sending data to Google Apps Script:', encoded);
-    xhr.send("sheetName=" + encodeURIComponent(sheetName) + "&" + encoded);
+      var url = "https://script.google.com/macros/s/AKfycbys49Yrl8m6Q5b6mMeDuNwqd_lxy1kGjkV31zT8T6oC05C4Chv6uqfDChAeh4lxUwru/exec";
+      var sheetName = form.dataset.sheet || "responses";
+
+      var xhr = new XMLHttpRequest();
+      xhr.open('POST', url);
+
+      xhr.onreadystatechange = function () {
+          if (xhr.readyState === 4 && xhr.status === 200) {
+              console.log('Form submitted successfully:', xhr.responseText);
+              form.reset();
+              document.getElementById("contact_form").classList.remove("opacity");
+              document.getElementById("contact_overlay").classList.add("display-none");
+              document.getElementById("form_title").classList.add("display-none");
+              document.getElementById("form").classList.add("display-none");
+              document.getElementById("thanks_message").classList.remove("display-none");
+
+              setTimeout(() => {
+                  document.getElementById("form_title").classList.remove("display-none");
+                  document.getElementById("form").classList.remove("display-none");
+                  document.getElementById("thanks_message").classList.add("display-none");
+              }, 3000);
+          } else if (xhr.readyState === 4) {
+              console.error('Error submitting form:', xhr.status, xhr.statusText);
+          }
+      };
+
+      document.getElementById("contact_form").classList.add("opacity");
+      document.getElementById("contact_overlay").classList.remove("display-none");
+
+      var encoded = Object.keys(data).map(function (k) {
+          return encodeURIComponent(k) + "=" + encodeURIComponent(data[k]);
+      }).join('&');
+      console.log('Sending data to Google Apps Script:', encoded);
+      xhr.send("sheetName=" + encodeURIComponent(sheetName) + "&" + encoded);
   }
 
   function loaded() {
-    var forms = document.querySelectorAll("form.gform");
-    for (var i = 0; i < forms.length; i++) {
-      forms[i].addEventListener("submit", handleFormSubmit, false);
-    }
+      var forms = document.querySelectorAll("form.gform");
+      for (var i = 0; i < forms.length; i++) {
+          forms[i].addEventListener("submit", handleFormSubmit, false);
+      }
   };
   document.addEventListener("DOMContentLoaded", loaded, false);
 
   function disableAllButtons(form) {
-    var buttons = form.querySelectorAll("button");
-    for (var i = 0; i < buttons.length; i++) {
-      buttons[i].disabled = true;
-    }
+      var buttons = form.querySelectorAll("button");
+      for (var i = 0; i < buttons.length; i++) {
+          buttons[i].disabled = true;
+      }
   }
 })();
